cf1adf7c2a186fd39cabc316bb19503f13cdb720
Matt Smith <matt.smith3108@gmail.com>
2022-09-15 16:36:43 +0100

    added production config file

---
+++ config/env/production/plugins.js
@@ -0,0 +1,43 @@
+module.exports = ({ env }) => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      introspection: true,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  "vercel-deploy": {
+    enabled: true,
+  },
+  placeholder: {
+    enabled: true,
+    config: {
+      size: 18,
+      lightness: 20,
+    },
+  },
+});
